06871e50d3770ad0af2fee24ba8ae04de562fe82
KDeepak5673 <deepakchandrashekhar0309@gmail.com>
2024-11-02 09:31:48 +0530

    minnor bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 
+
+// import { GoogleOAuthProvider } from '@react-oauth/google';
+
+
 //Component
 import Messenger from "./components/Messenger";
 
 function App() {
+
+  // const clientId = '363975543480-b19ujq3ho6igccluhfne7e91fhhp8vvj.apps.googleusercontent.com';
+
   return (
     <div >
       <Messenger/>
